ce35971f130ad02457a017533c1a89c0e035fc4c
shyam020 <patelshyam18352@gmail.com>
2024-11-11 21:11:42 +0530

    add project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,46 +1,31 @@
 import React from 'react';
-// import todolist from "../assects/gfg.png";
-import Docsmini from  "../assects/Docs-mini.png";
+import todolist from "../assects/gfg.png";
+import Docsmini from "../assects/Docs-mini.png";
+import ezyResume from "../assects/ezyResume.png";
 
 const Project = () => {
 
   const projects = [
     {
       id: 1,
+      src: ezyResume,
+      link: 'https://ezy-resume.netlify.app/',
+      },
+ 
+    {
+      id: 2,
       src: Docsmini,
       link: 'https://github.com/shyam020/Docs-mini',
          websiteLink: 'https://docs-mini.netlify.app/',
     },
-  //   {
-  //     id: 2,
-  //     src: todolist,
-  //     link: 'https://github.com/shyam020',
-  //         websiteLink: 'https://www.linkedin.com/in/shyam-patel-1312a62a8/',
-  //   },
-  //   {
-  //     id: 3,
-  //     src: todolist,
-  //     link: 'https://github.com/shyam020',
-  //         websiteLink: 'https://www.linkedin.com/in/shyam-patel-1312a62a8/',
-  //   },
-  //   {
-  //     id: 4,
-  //     src: todolist,
-  //     link: 'https://github.com/shyam020',
-  //         websiteLink: 'https://www.linkedin.com/in/shyam-patel-1312a62a8/',
-  //   }, {
 
-  //     id: 5,
-  //     src: todolist,
-  //     link: 'https://github.com/shyam020',
-  //         websiteLink: 'https://www.linkedin.com/in/shyam-patel-1312a62a8/',
-  //   },
-  // {
-  //          id: 6,
-  //   src: todolist,
-  //   link: 'https://github.com/shyam020',
-  //         websiteLink: 'https://www.linkedin.com/in/shyam-patel-1312a62a8/',
-  //   },
+      {
+        id: 3,
+        src: todolist,
+        link: 'https://github.com/shyam020',
+            websiteLink: 'https://www.linkedin.com/in/shyam-patel-1312a62a8/',
+    },
+  
   ]
   return (
     <div name="projects" className=' h-full  w-full bg-gradient-to-b from-black via-black to-gray-900  text-white'>
@@ -54,7 +39,7 @@ const Project = () => {
           projects.map(({ id, src,link,websiteLink }) => (
             
           <div key={id} className='shadow-md shadow-gray-600 rounded-lg '>
-            <img src={ src } alt="" srcset="" className='rounded-md duration-200 hover:scale-105' />
+            <img src={ src } alt="" srcset="" className=' sm:h-[250px] lg:rounded-md duration-200 hover:scale-105 h-[150px] w-full object-cover  ' />
             <div className='flex items-center justify-center'>
             <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'onClick={()=>window.open(websiteLink, "_blank")}>Website</button>
               <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'  onClick={() => window.open(link, "_blank")}>Source</button>
